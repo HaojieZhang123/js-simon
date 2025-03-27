26c8a2bedd5b44e2215b962847ffa6540bbef2d8
HaojieZhang123 <haojiezhang123@gmail.com>
2025-03-27 15:23:01 +0100

    [UPD] saving user answer in an array, in preparation for result confrontations

--- js/script.js
+++ js/script.js
@@ -16,6 +16,7 @@
 const numbers = [];
 const answers = [];
 const countdown = document.getElementById('countdown');
+const instructions = document.getElementById('instructions');
 const numbersList = document.getElementById('numbers-list');
 const answersForm = document.getElementById('answers-form');
 const button = document.querySelector('button');
@@ -30,36 +31,52 @@ function compareAnswers(answers, numbers){
     let result = [];
     for (let i = 0; i < answers.length; i++){
         for (let j = 0; j < numbers.length; j++){
-            if (answers[i] === numbers[j]){
+            if (answers[i] == numbers[j]){
+                console.log(`Trovato il numero ${answers[i]}`);
                 result.push(answers[i]);
                 // dopo aver trovato il numero corrispondente lo rimuovo dall'array per evitare ripetizioni
                 numbers.splice(j, 1);
             }
         }
     }
+    console.log(`risultato funzione: ${result}`);
     return result;
 }
 
 // countdown
-let count = 10;
+let count = 3;
 let interval = setInterval(() => {
    countdown.innerHTML = count;
     count--;
     if (count === 0){
         clearInterval(interval);
-        countdown.innerHTML = count;
+        countdown.innerHTML = '';
+        instructions.innerHTML = 'Inserisci i numeri!';
         document.querySelector('#answers-form').classList.remove('d-none');
     }
 }, 1000);
 
 // generazione numeri randomici e salvataggio in array numbers
 for (let i = 0; i < 5; i++){
-    numbers.push(RNG(0,99));
+    numbers.push(RNG(1, 50));
     numbersList.innerHTML += `<li>${numbers[i]}</li>`;
 }
-console.log(numbers);
+console.log(`numeri generati: ${numbers}`);
 
 // cancellazione numeri dallo schermo allo scadere del countdown
 setTimeout(() => {
     numbersList.innerHTML = '';
 }, (count * 1000));
+
+button.addEventListener('click', function(event){
+    event.preventDefault();
+    // salvataggio delle risposte in form nell'array answers
+    answers.push(document.querySelector('input').value);
+    answers.push(document.querySelector('input:nth-child(2)').value);
+    answers.push(document.querySelector('input:nth-child(3)').value);
+    answers.push(document.querySelector('input:nth-child(4)').value);
+    answers.push(document.querySelector('input:nth-child(5)').value);
+
+    console.log(`risposte inserite: ${answers}`);
+
+})
